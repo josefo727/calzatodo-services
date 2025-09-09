8b8aea23644366e03efd891df92947d8b35f9556
José R. Gutierrez <josefo727@gmail.com>
2025-09-09 13:44:26 -0500

    Remove unused middlewares and update dependencies

--- node/clients/index.ts
+++ node/clients/index.ts
@@ -1,10 +1,15 @@
 import { IOClients } from '@vtex/api'
 
 import MasterData from './masterData'
+import OMS from './oms'
 
 // Extend the default IOClients implementation with our own custom clients.
 export class Clients extends IOClients {
   public get masterData() {
     return this.getOrSet('masterData', MasterData)
+  }
+
+  public get oms() {
+    return this.getOrSet('oms', OMS)
   }
 }
--- node/index.ts
+++ node/index.ts
@@ -2,38 +2,21 @@ import type { ClientsConfig, ServiceContext, RecorderState } from '@vtex/api'
 import { LRUCache, method, Service } from '@vtex/api'
 
 import { Clients } from './clients'
-import { auth } from './middlewares/auth'
-import {
-  createDocument,
-  getDocument,
-  updateDocument,
-  deleteDocument,
-  listDocuments
-} from './middlewares/masterData'
+import { checkCoupon } from './middlewares/coupon'
 
 const TIMEOUT_MS = 30000
 
-// Create a LRU memory cache for the Status client.
-// The 'max' parameter sets the size of the cache.
-// The @vtex/api HttpClient respects Cache-Control headers and uses the provided cache.
-// Note that the response from the API being called must include an 'etag' header
-// or a 'cache-control' header with a 'max-age' value. If neither exist, the response will not be cached.
-// To force responses to be cached, consider adding the `forceMaxAge` option to your client methods.
 const memoryCache = new LRUCache<string, any>({ max: 5000 })
 
 metrics.trackCache('status', memoryCache)
 
-// This is the configuration for clients available in `ctx.clients`.
 const clients: ClientsConfig<Clients> = {
-  // We pass our custom implementation of the clients bag, containing the Status client.
   implementation: Clients,
   options: {
-    // All IO Clients will be initialized with these options, unless otherwise specified.
     default: {
       retries: 2,
       timeout: TIMEOUT_MS,
     },
-    // This key will be merged with the default options and add this cache to our Status client.
     status: {
       memoryCache,
     },
@@ -41,34 +24,18 @@ const clients: ClientsConfig<Clients> = {
 }
 
 declare global {
-  // We declare a global Context type just to avoid re-writing ServiceContext<Clients, State> in every handler and resolver
   type Context = ServiceContext<Clients, State>
 
-  // The shape of our State object found in `ctx.state`. This is used as state bag to communicate between middlewares.
   interface State extends RecorderState {
     code: number
   }
 }
 
-// Export a service that defines route handlers and client options.
 export default new Service({
   clients,
   routes: {
-    // Master Data routes
-    createDocument: method({
-      POST: [auth, createDocument],
-    }),
-    getDocument: method({
-      GET: [auth, getDocument],
-    }),
-    updateDocument: method({
-      PUT: [auth, updateDocument],
-    }),
-    deleteDocument: method({
-      DELETE: [auth, deleteDocument],
-    }),
-    listDocuments: method({
-      GET: [auth, listDocuments],
+    checkCoupon: method({
+      POST: [checkCoupon],
     }),
   },
 })
